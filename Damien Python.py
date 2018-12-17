5cbe8997f4210ad29c31ec0e12df108a77cee141
Damien Bamien <cheudami@pvncvc.ca>
2018-12-17 14:01:16 -0500

    daminwrojbg

--- Damien Python.py
+++ Damien Python.py
@@ -1,15 +1,44 @@
 import os
 
+def start():
+        print('Start: ')
+        print('')
+        x=int(input('(1/2) 1. Create account  2. Login '))
+        if x==1:
+              createAccount()
+        elif x==2:
+              login()
+        else:
+              print(x, 'was not an option')
+              
+
 def createAccount():
         global username
         global password
-        global initialDeposit
+        global chequingBalance
+        global savingsBalance
+
+        chequingBalance=0
+        savingsBalance=0
         username = input('Username: Create a username: ')
         password = str(input('Password: Enter your 4 digit PIN number: '))
-        selectAccount()
-        initialDeposit =input('Initial Deposit: Enter your initial deposit: ')
-        f = open('passwords.txt', 'w')
-        f.write(username+ ' ' +password+ ' ' +initialDeposit +'\n')
+        print('Make an initial deposit. Select an account: ')
+        print('')
+        x=int(input('(1/2):   1. Chequing  2. Savings '))
+        print('')
+        deposit=int(input('Input the value of your deposit in dollars: '))
+        if x==1:
+                chequingBalance= deposit
+                print('Chequing balance: ', chequingBalance)
+                savingsBalance=0
+        elif x==2:
+                savingsBalance= deposit
+                print('Savings balance: ', savingsBalance)
+                chequingBalance=0
+        else:
+                print('Input unavailable. ')
+        f = open('passwords.txt', 'a')
+        f.write(username+ ' ' +password+ ' ' +str(chequingBalance) + ' ' + str(savingsBalance) + '\n')
         
       
 
@@ -28,18 +57,29 @@ def login():
                                 print('Incorrect Password ')
                 elif username != data[0]:
                         print('This username does not exist')
+
+
 def initialDeposit():
-        print('Make an iniital deposit: ')
-        print('Select an account for the initial deposit: ')
-        x=input('1. 
+        print('Make an initial deposit. Select an account: ')
+        print('')
+        x=input('(1/2):   1. Chequing  2. Savings ')
+        print('')
+        deposit=int(input('Input the value of your deposit in dollars: '))
+        if x==1:
+                chequingBalance= deposit
+                print('Chequing balance: ')
+                print(chequingBalance)
+        elif x==2:
+                savingsBalance= deposit
+                print('Savings balance: ')
+                print(savingsBalance)
 
         
 def selectAccount():
-        x=input('Select Account:  1. Chequing   2. Savings ')
+        x=input('Select Account(1/2):  1. Chequing   2. Savings ')
         if x == 1:
                 print(chequingBalance)
         elif x == 2:
                 print(savingsBalance)
-
-createAccount()
-print('x')
+              
+start()
