ae5a0a9b1e2437b8a4f58b405b5080e5f35e37e9
Damien Bamien <cheudami@pvncvc.ca>
2019-01-11 13:13:07 -0500

    rhyq

--- Damien Python.py
+++ Damien Python.py
@@ -26,29 +26,44 @@ def createAccount():
 
         chequingBalance=0
         savingsBalance=0
-        username = input('Username: Create a username: ')
+        username = input('Create a username: ')
         password = str(input('Password: Enter your 4 digit PIN number: '))
-        print('Make an initial deposit. Select an account: ')
-        print('')
-        x=int(input('(1/2):   1. Chequing  2. Savings '))
-        print('')
-        deposit=int(input('Input the value of your deposit in dollars: '))
-        if x==1:
-                chequingBalance= deposit
-                print('Chequing balance: ', chequingBalance)
-                savingsBalance=0
-        elif x==2:
-                savingsBalance= deposit
-                print('Savings balance: ', savingsBalance)
-                chequingBalance=0
-        else:
-                print('Input unavailable. ')
-        f = open('passwords.txt', 'a')
-        f.write(username+ ' ' +password+ ' ' +str(chequingBalance) + ' ' + str(savingsBalance) + '\n')
-        
+        w=True
+        x=0
+        while w==True:
+                while x>=0:
+                        if 1000<= int(password) <= 9999:
+                                print('Make an initial deposit. Select an account: ')
+                                print('')
+                                x=int(input('(1/2):   1. Chequing  2. Savings '))
+                                print('')
+                                deposit=int(input('Input the value of your deposit in dollars: '))
+                                if x==1:
+                                        chequingBalance= deposit
+                                        print('Chequing balance: ', chequingBalance)
+                                        savingsBalance=0
+                                elif x==2:
+                                        savingsBalance= deposit
+                                        print('Savings balance: ', savingsBalance)
+                                        chequingBalance=0
+                                else:
+                                        print('Input unavailable. ')
+                                f = open('passwords.txt', 'a')
+                                f.write(username+ ' ' +password+ ' ' +str(chequingBalance) + ' ' + str(savingsBalance) + '\n')
+                                
+                        if w==True:
+                                print('Create a 4 digit password. ')
+                                x-=1
+                                
+                                
       
 
 def login():
+        global chequingBalance
+        global savingsBalance
+        global username
+        global password
+        global passwordLocation
         file = open('./passwords.txt').read()
         file=file.split("\n")
         login=True
@@ -56,29 +71,36 @@ def login():
         x=3
         while login==True:
                 while x>=0:
-                        for n in file:
-                                data = n.split(" ")
-                                username=str(input('Enter your username: '))
+                        username=str(input('Enter your username: '))
+                        userFound = False
+                        for passwordLocation in range(len(file)):
+                                data = file[passwordLocation].split(" ")
                                 if data[0] == username:
-                                        while password==True:
+                                        userFound = True
+                                        while password==True and x>0:
                                                 password=input('Enter your 4 digit PIN password: ')
                                                 if password == data[1]:
                                                         print('Login Successful ')
                                                         password=False
                                                         login=False
+                                                        chequingBalance= float(data[2])
+                                                        savingsBalance= float(data[3])
+                                                        username= data[0]
+                                                        password= data[1]
                                                         return
                                                 elif password != data[1]:
                                                         print('Incorrect Password ')
                                                         print('Re-enter your password')
                                                         password=True
                                                         x-=1
-                                else:
-                                        print('This username does not exist')
-                                        x-=1
+                        if userFound == False:
+                                print('This username does not exist')
+                                x-=1
 
         
 def printBalance():
-        x=input('Select Account(1/2):  1. Chequing   2. Savings ')
+        
+        x=int(input('Select Account(1/2):  1. Chequing   2. Savings '))
         if x == 1:
                 print(chequingBalance)
         elif x == 2:
@@ -99,6 +121,7 @@ Enter(1/2/3): '''))
                         if withdrawal <= chequingBalance:
                                 print('Current Chequing account balance: ', chequingBalance)
                                 chequingBalance= chequingBalance - withdrawal
+                                update()
                                 print('New Chequing account balance: ', chequingBalance)
                                 print('You have successfully withdrawn ', withdrawal, ' dollars')
                         else:
@@ -108,6 +131,7 @@ Enter(1/2/3): '''))
                         if withdrawal <= savingsBalance:
                                 print('Current Savings account balance: ', savingsBalance)
                                 savingsBalance= savingsBalance - withdrawal
+                                update()
                                 print('New Savings account balance: ', savingsBalance)
                                 print('Your deposit of: ', withdrawal, 'dollars was successful')
                         else:
@@ -133,12 +157,14 @@ Enter(1/2/3):  '''))
                         print('Current Chequing account balance: ', chequingBalance)
                         deposit=float(input('Input the value of your deposit: '))
                         chequingBalance=chequingBalance+deposit
+                        update()
                         print('New Chequing account balance: ', chequingBalance)
                         print('Your deposit of ', deposit, 'dollars was successful.')
                 elif x==2:
                         print('Current Savings account balance: ', savingsBalance)
                         deposit=float(input('Input the value of your deposit: '))
                         savingsBalance=savingsBalance+deposit
+                        update()
                         print('New Savings account balance: ', savingsBalance)
                 elif x==3:
                         n=False
@@ -163,6 +189,7 @@ Enter(1/2/3):  '''))
                 else:
                         chequingBalance= chequingBalance-transferAmount
                         savingsBalance=savingsBalance+transferAmount
+                        update()
                         print('You have successfully transferred ', transferAmount, 'from Chequings to Savings.')
                         print('''
 New Chequing account balance: ''', chequingBalance)
@@ -176,14 +203,28 @@ New Chequing account balance: ''', chequingBalance)
                 else:
                         chequingBalance= chequingBalance+transferAmount
                         savingsBalance=savingsBalance-transferAmount
+                        update()
                         print('You have successfully transferred ', transferAmount, 'from Savings to Chequing.')
                         print('''
 New Savings account balance: ''', savingsBalance)
                         print('New Chequing account balance: ', chequingBalance)
+
+def update():
+        global passwordLocation
+        file = open('./passwords.txt').read()
+        file=file.split("\n")[:-1]
+
+        f = open('passwords.txt', 'w')
+
+        for n in range(len(file)):
+                if n == passwordLocation:
+                        continue
+                f.write(file[n] + '\n')
         
-def checkBalance():
-        print('Chequing Balance: ',chequingBalance)
-        print('Savings Balance: ',savingsBalance)
+        f.write(username+ ' ' +password+ ' ' +str(chequingBalance) + ' ' + str(savingsBalance) + '\n')
+        f.close()
+        
+        passwordLocation = len(file)-1
                 
 def option():
         x=True
@@ -193,8 +234,7 @@ def option():
 2. Deposit
 3. Transfer Money
 4. E-transfer
-5. Check B
-alance
+5. Print Balance
 6. Logout''')
                 y=int(input('--->'))
                 if y==1:
@@ -204,18 +244,45 @@ alance
                 elif y==3:
                         transferMoney()
                 elif y==4:
-                        e-transfer()
+                        eTransfer()
                 elif y==5:
-                        checkBalance()
+                        printBalance()
                 elif y==6:
                         print('Thank you for banking with bank')
                         print('Bye bye now *waving hand*')
                         break
+                else:
+                        print(y,'was not an option idiot head dumb face stupid head')
 
+def eTransfer():
+        file = open('./passwords.txt').read()
+        file=file.split("\n")
+        y=True
+        while y==True:
+                for n in file:
+                        data = n.split(" ")
+                        name=input('Enter the username of the person you would like to E-Transfer money to:')
+                        if name == data[0]:
+                                print('You are sending money from your chequing account')
+                                money=input('How much money would you like to send to '+name+"?")
+                                if money>data[2]:
+                                        print('Insufficient funds')
+                                        return
+                                if money<data[2]:
+                                        data[2]= int(data[2]) +money
+                                        data[2]= int(data[2]) -money
+                                        print('You have successfully transfered $' +money+ ' to' +name)
+                                        y=False
+                                else:
+                                        print('That is not an account, which bank do toy bank at?')
+                                
+                        
+                        elif name != data[0]:
+                                print('That account does not exist')
+                                return
+
+        
+
+print('Welcome to the Bank. ')
 start()
-file = open('./passwords.txt', 'r')
-print .readline(): 
-
-
 option()
-
