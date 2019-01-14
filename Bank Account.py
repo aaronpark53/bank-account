b519b6dfef7235d1635bc82356b180201e14d5dc
Aaron Park <aaronpark53@icloud.com>
2019-01-14 13:37:51 -0500

    bblblblbl

--- Bank Account.py
+++ Bank Account.py
@@ -1,10 +1,23 @@
-import os
+#Aaron Park and Damien Cheung
+#December 12 2018 - January 11 2019
+#Purpose: This is a bank account program. In this program, the user inputs
+#all the information needed to run the program. This includes username,
+#password, how much money they would like to deposit or withdraw, and what
+#function they would like to use. This program has a file attatched to it
+#that contains all the usernames, passwords, and the amount of money in
+#the users accounts. This program contains many if/elif/else statements,
+#functions for all the necessary options, and for and while loops. 
 
-def start():
+
+
+
+import os #this allows us to open and use our passwords file in the program
+
+def start(): #this is the start function that will first appear when the program is ran. 
         print('Start: ')
         print('')
         y=True
-        while y==True:
+        while y==True: #a while loop that will repeat until the user logs in or creates an account
                 x=int(input('(1/2) 1. Create account  2. Login '))
                 if x==1:
                       createAccount()
@@ -15,6 +28,7 @@ def start():
                 else:
                       print(x, 'was not an option idiot head dumb face stupid head')
                       print('Try again')
+                      # an if/else/elif statement that the user uses to either log in or create an account
               
               
 
@@ -22,32 +36,33 @@ def createAccount():
         global username
         global password
         global chequingBalance
-        global savingsBalance
+        global savingsBalance #global variables that are used in other functions
         chequingBalance=0
-        savingsBalance=0
+        savingsBalance=0 #setting the balance of both accounts to 0.
         username = input('Create a username: ')
-        password = str(input('Password: Enter your 4 digit PIN number: '))
+        password = str(input('Password: Enter your 4 digit PIN number: ')) #creating a username and password
         if int(password) >=10000 or int(password) <=999:
                 print("That's not 4, can you count?????")
-                start()
+                start() #if the password is not 4 digits, the user will go back to the start screen
         print('Make an initial deposit. Select an account: ')
         print('')
         x=int(input('(1/2):   1. Chequing  2. Savings '))
         print('')
-        deposit=int(input('Input the value of your deposit in dollars: '))
+        deposit=int(input('Input the value of your deposit in dollars: ')) #making an initial deposit for the user's account
         if x==1:
                 chequingBalance= deposit
                 print('Chequing balance: ', chequingBalance)
-                savingsBalance=0
+                savingsBalance=0 #making a deposit into a chequings account
         elif x==2:
                 savingsBalance= deposit
                 print('Savings balance: ', savingsBalance)
-                chequingBalance=0
+                chequingBalance=0 #making a deposit into a savings account
         else:
-                print('Input unavailable. ')
-        f = open('passwords.txt', 'a')
-        f.write(username+ ' ' +password+ ' ' +str(chequingBalance) + ' ' + str(savingsBalance) + '\n')
-        return
+                print('Input unavailable. ') #this will show if the user does input 1 or 2
+        f = open('passwords.txt', 'a') #opening the file
+        f.write(username+ ' ' +password+ ' ' +str(chequingBalance) + ' ' + str(savingsBalance) + '\n') #this adds all their information to the passwords file
+        f.close()
+        return #returning to the options function
                                   
 
 def login():
@@ -55,41 +70,41 @@ def login():
         global savingsBalance
         global username
         global password
-        global passwordLocation
+        global passwordLocation #global variables that are used in other functions
         file = open('./passwords.txt').read()
         file=file.split("\n")
         login=True
-        password=True
-        x=3
+        password=True #setting up while loops
+        x=3 #this is here so that the user only has four tries to log in 
         while login==True:
                 while x>=0:
-                        username=str(input('Enter your username: '))
+                        username=str(input('Enter your username: ')) #where the user inputs their username
                         userFound = False
                         for passwordLocation in range(len(file)):
                                 data = file[passwordLocation].split(" ")
-                                if data[0] == username:
+                                if data[0] == username: #data[0] is the column where all the usernames are kept. this also checks if the user input is a real username
                                         userFound = True
                                         while password==True and x>0:
-                                                password=input('Enter your 4 digit PIN password: ')
-                                                if password == data[1]:
+                                                password=input('Enter your 4 digit PIN password: ') #where the user inputs their password  is the column where all the usernames are kept. this also checks if the user input is a real username
+                                                if password == data[1]: #data[1] is the column where all the passwords are kept. this also checks if the user input is the right password. 
                                                         print('Login Successful ')
                                                         password=False
                                                         login=False
-                                                        chequingBalance= float(data[2])
-                                                        savingsBalance= float(data[3])
+                                                        chequingBalance= float(data[2]) #data[2] is the column where the chequing balance is kept
+                                                        savingsBalance= float(data[3]) #date[3] is the column where the savings balance is kept.
                                                         username= data[0]
                                                         password= data[1]
                                                         return
-                                                elif password != data[1]:
+                                                elif password != data[1]: #if the password entered does not match up to the one in the file associated with their username they will re-enter their password
                                                         print('Incorrect Password ')
                                                         print('Re-enter your password')
                                                         password=True
                                                         x-=1
                         if userFound == False:
-                                print('This username does not exist')
-                                x-=1
+                                print('This username does not exist') #this will show if the input does not match up any of the usernames in the file.
+                                x-=1  
 
-def option():
+def option(): #this is the main function of the program, it directs the user to all the different function they can use in a bank account.
         x=True
         while x==True:
                 print('What would you like to do?')
@@ -106,35 +121,35 @@ def option():
                 elif y==3:
                         transferMoney()
                 elif y==4:
-                        printBalance()
+                        printBalance() #series of if/elif statements that the user uses to decide what to do.
                 elif y==5:
-                        print('Thank you for banking with the Bank')
-                        break
+                        print('Thank you for banking with the Bank') 
+                        break #this break is used to log out
                 else:
-                        print(y,'was not an option idiot head dumb face stupid head')
+                        print(y,'was not an option idiot head dumb face stupid head') 
 
 def withdraw():
         global chequingBalance
-        global savingsBalance
+        global savingsBalance #global variables that are used in other functions
         n=True
         while n==True:
                 x=int(input('''Select account to withdraw from:
 1. Chequing
 2. Savings
 3. Go Back:
-Enter(1/2/3): '''))
+Enter(1/2/3): ''')) #this is where the user selects which account they want to withdraw money
                 if x==1:
                         withdrawal=float(input('Input value of the withdrawal: '))
-                        if withdrawal <= chequingBalance:
+                        if withdrawal <= chequingBalance: #if they withdrew less money than they have in their account, the program will continue
                                 print('Current Chequing account balance: ', chequingBalance)
                                 chequingBalance= chequingBalance - withdrawal
-                                update()
+                                update() #updating their chequing balance
                                 print('New Chequing account balance: ', chequingBalance)
-                                print('You have successfully withdrawn ', withdrawal, ' dollars')
-                                return
+                                print('You have successfully withdrawn ', withdrawal, ' dollars') 
+                                return #printing their new chequing balance and returning the user to the options menu
                         else:
                                 print('Insufficient Funds.')
-                                return
+                                return #this will show if the user tries to steal money from the bank and withdraw more money than they have, and return them to the options main menu.
                 elif x==2:
                         withdrawal=int(input('Input value of the withdrawal: '))
                         if withdrawal <= savingsBalance:
@@ -143,72 +158,73 @@ Enter(1/2/3): '''))
                                 update()
                                 print('New Savings account balance: ', savingsBalance)
                                 print('Your deposit of: ', withdrawal, 'dollars was successful')
-                                return
+                                return #same thing but for savings account
                         else:
                                 print('Insufficient Funds.')
                                 return
                 elif x==3:
                         n=False
-                        return
+                        return #if they choose 3, they will go back to the options menu
                 else:
-                        print('That is not an option. Try again. ')
+                        print('That is not an option. Try again. ') #this will print if the user does not input 1, 2, or 3 
                 
 
 def deposit():
         global chequingBalance
-        global savingsBalance
+        global savingsBalance #global variables that are used in other functions
         n=True
         while n==True:
                 x=int(input('''Select account to deposit into:
 1. Chequing
 2. Savings
 3. Go Back:
-Enter(1/2/3):  '''))
+Enter(1/2/3):  ''')) #this is where the user selects which account they want to deposit money to
                 if x==1:
                         print('Current Chequing account balance: ', chequingBalance)
-                        deposit=float(input('Input the value of your deposit: '))
+                        deposit=float(input('Input the value of your deposit: ')) #where the user inputs their deposit
                         chequingBalance=chequingBalance+deposit
-                        update()
+                        update() #updating their chequing balance
                         print('New Chequing account balance: ', chequingBalance)
-                        print('Your deposit of ', deposit, 'dollars was successful.')
-                        return
+                        print('Your deposit of ', deposit, 'dollars was successful.') 
+                        return #printing their new chequing balance and returning the user to the options menu
                 elif x==2:
                         print('Current Savings account balance: ', savingsBalance)
                         deposit=float(input('Input the value of your deposit: '))
                         savingsBalance=savingsBalance+deposit
                         update()
                         print('New Savings account balance: ', savingsBalance)
-                        return
+                        return #same thing for savings account
                 elif x==3:
                         n=False
-                        return
+                        return #if they choose 3, they will go back to the options menu
                 else:
-                        print('That is not an option. Try again.')
+                        print('That is not an option. Try again.') #this will print if the user does not input 1, 2, or 3
 
-def transferMoney():
-        global chequingBalance
-        global savingsBalance
+
+def transferMoney(): #this is a function that allows you to transfer money between your own chq/sav accounts
+        global chequingBalance 
+        global savingsBalance #these global statements allow you to use these variables outside of the function
         x=int(input('''Select option:
 1. Chequing to Savings 
 2. Savings to Chequing 
 3. Go Back             
-Enter(1/2/3):  '''))
+Enter(1/2/3):  ''')) #x is the variable that represents the users choice and it will correspond with the correct option which is displayed with the print statement
         if x==1:
                 print('Current Chequing account balance: ',chequingBalance)
-                print('Current Savings account balance: ', savingsBalance)
-                transferAmount=float(input('Input the value of the transfer: '))
+                print('Current Savings account balance: ', savingsBalance) #displaying the current accounts balance
+                transferAmount=float(input('Input the value of the transfer: ')) #input for the amount of money that is going to be transferred
                 if transferAmount > chequingBalance:
                         print('Insufficient funds. ')
-                        return
+                        return #if the transfer amount is more than the user owns in the account that is transferring money, the program will say that there is insufficient funds and it will relaunch the option
                 else:
-                        chequingBalance= chequingBalance-transferAmount
+                        chequingBalance= chequingBalance-transferAmount #if the amount is acceptable, the chequing balance will lose that amount of money and the savings account will gain that money, simulating the transfer between accounts.
                         savingsBalance=savingsBalance+transferAmount
-                        update()
+                        update() #update file function
                         print('You have successfully transferred ', transferAmount, 'from Chequings to Savings.')
                         print('''
 New Chequing account balance: ''', chequingBalance)
                         print('New Savings account balance: ', savingsBalance)
-                        return
+                        return #a few print statements that explain what happened, then return to options menu.
         elif x==2:
                 print('Current Savings account balance: ', savingsBalance)
                 print('Current Chequing account balance: ', chequingBalance)
@@ -225,45 +241,47 @@ New Chequing account balance: ''', chequingBalance)
                         print('''
 New Savings account balance: ''', savingsBalance)
                         print('New Chequing account balance: ', chequingBalance)
-                        return
+                        return #the same coding but reversed for the savings to chq option rather than cheq to savings
         elif x==3:
-                return
+                return #if the user wants to go back, the options menu will appear
         else:
-                print('That is not an option. ')
+                print('That is not an option. ') #if the user doesnt input 1/2/3, this message appears
                 
-def printBalance():
+def printBalance(): #function that will allow the user to check their account balances
 
-        x=int(input('Select Account(1/2):  1. Chequing   2. Savings '))
+        x=int(input('Select Account(1/2):  1. Chequing   2. Savings ')) #displaying the options 
         if x == 1:
-                print(chequingBalance)
+                print(chequingBalance) #if the user enters 1, the chequing balance will be displayed
         elif x == 2:
-                print(savingsBalance)
+                print(savingsBalance) #if 2 is entered, savings balance is displayed
 
 
                 
 
-def update():
-        global passwordLocation
-        file = open('./passwords.txt').read()
-        file=file.split("\n")[:-1]
+def update(): #this is a function that updates the info in the passwords file and removes the old information
+        global passwordLocation #this variable is open to use in other coding
+        file = open('./passwords.txt').read() #the code opens the passwords file and reads it
+        file=file.split("\n")[:-1] #file is split and read in new lines so that each accounts information is seperately stored
 
-        f = open('passwords.txt', 'w')
+        f = open('passwords.txt', 'w') #the file is opened and is permitted to write over
 
         for n in range(len(file)):
                 if n == passwordLocation:
-                        continue
-                f.write(file[n] + '\n')
+                        continue #continue if the scan finds the correct password location (which is stored in an earlier function (create accoutn/login))
+                f.write(file[n] + '\n') #a new line is written
         
         f.write(username+ ' ' +password+ ' ' +str(chequingBalance) + ' ' + str(savingsBalance) + '\n')
-        f.close()
+        f.close() #all the updated account data is written into the newest line then the file is closed
         
-        passwordLocation = len(file)-1
+        passwordLocation = len(file)-1 # the new location is updated
                 
 
 
 
 
 
-print('Welcome to the Bank. ')
-start()
-option()
+print('Welcome to the Bank. ') #opening statement
+start() #launching the start function. If it is successfully completed,
+option() #the options menu will launch and run continuously until the user logout
+
+
